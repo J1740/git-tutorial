f9a9bc0560fb88584fa49dded9450d2ed0fa205a
J1740 <akita9260sato@gmail.com>
2020-10-30 14:26:20 +0900

    second comit

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 for 1 in 1..100 do
- if i % 3
+ if i % 3 == 0
   print "Fizz\n"
- elsif i % 5
+ elsif i % 5 == 0
   print "Buzz\n"
- elsif i % 3 && i % 5 
+ elsif i % 3 == 0 && i % 5 == 0 
   print "FizzBuzz\n"
  else
   print "i\n"
